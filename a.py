e3cb496118589ab79fe57f3386f9bd0ea915f505
16808910041680 <forforfirefire@gmail.com>
2025-05-25 10:39:50 +1000

    popm

---
+++ a.py
@@ -0,0 +1,14 @@
+a = input ("Please give me a letter:")
+for i in a:
+    if a == 'a':
+        print("You entered the letter 'a'.")
+        break
+    elif a == 'b':
+        print("You entered the letter 'b'.")
+        break
+    elif a == 'c':
+        print("You entered the letter 'c'.")
+        break
+    else:
+        print("You entered a letter that is not 'a', 'b', or 'c'.")
+        break
