620bd1805635d4983161fe224efbc5648f3336d4
16808910041680 <forforfirefire@gmail.com>
2025-05-25 10:45:07 +1000

    iuo

---
+++ jioa.py
@@ -0,0 +1,9 @@
+for x in range (10242936410297374892):
+    if x % 20 == 0:
+        print("twist")
+    elif x % 15 == 0:
+        pass
+    elif x % 5 == 0:
+        print("shout")
+    else:
+        print(x)
